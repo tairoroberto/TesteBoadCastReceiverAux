8d11962340ea9332652d6c5f6eead59ac5c7f775
tairoroberto <github@nowsolucoes.com.br>
2014-12-16 21:27:13 -0200

    projeto para auxiliar no teste com BroadcastReceiver - Tairo

---
+++ gen/com/example/testeboadcastreceiveraux/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.testeboadcastreceiveraux;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
